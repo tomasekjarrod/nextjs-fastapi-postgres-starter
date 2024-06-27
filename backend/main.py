67b1a78d5657c33900958b8abe5c0a38b0c85ddc
Jarrod <jtomasek@stedwards.edu>
2024-06-27 15:11:54 -0500

    enabled cors

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.orm import joinedload
@@ -11,6 +12,14 @@ from seed import seed_user_if_needed
 seed_user_if_needed()
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/users/me")
 async def get_my_user():
