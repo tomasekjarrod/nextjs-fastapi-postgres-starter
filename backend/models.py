3ae6c67bdc19c0388773455817d9489c1ca3564a
Jarrod <jtomasek@stedwards.edu>
2024-06-27 13:09:16 -0500

    defined models

--- backend/models.py
+++ backend/models.py
@@ -1,8 +1,5 @@
-from sqlalchemy import String
-from sqlalchemy.orm import DeclarativeBase
-from sqlalchemy.orm import Mapped
-from sqlalchemy.orm import mapped_column
-
+from sqlalchemy import String, DateTime, Integer, ForeignKey, desc, func
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
 
 class Base(DeclarativeBase):
     pass
@@ -14,5 +11,47 @@ class User(Base):
     id: Mapped[int] = mapped_column(primary_key=True)
     name: Mapped[str] = mapped_column(String(30))
 
+    # Relationship to threads created by the user
+    threads = relationship('Thread', back_populates='user')
+
+    # Relationship to messages sent by the user
+    messages = relationship('ThreadMessage', back_populates='sender')
+
     def __repr__(self) -> str:
-        return f"User(id={self.id!r}, name={self.name!r}"
+        return f"User(id={self.id!r}, name={self.name!r})"
+
+
+class Thread(Base):
+    __tablename__ = "thread"
+    
+    id: Mapped[int] = mapped_column(primary_key=True)
+    created_by: Mapped[int] = mapped_column(Integer, ForeignKey('user.id'))
+    created_at: Mapped[DateTime] = mapped_column(DateTime, default=func.now())
+
+    # Relationship to user
+    user = relationship('User', back_populates='threads')
+    
+    # Relationship to children thread messages, ordered by created_at desc
+    messages = relationship('ThreadMessage', order_by=desc('ThreadMessage.created_at'), back_populates='thread')
+
+    def __repr__(self) -> str:
+        return f"Thread(id={self.id!r}, created_at={self.created_at!r}, created_by={self.created_by!r})"
+
+
+class ThreadMessage(Base):
+    __tablename__ = "thread_message"
+    
+    id: Mapped[int] = mapped_column(primary_key=True)
+    content: Mapped[str] = mapped_column(String())
+    sender_id: Mapped[int] = mapped_column(Integer, ForeignKey('user.id'), nullable=True) # Nullable to indicate AI message vs user message
+    thread_id: Mapped[int] = mapped_column(Integer, ForeignKey('thread.id'))
+    created_at: Mapped[DateTime] = mapped_column(DateTime, default=func.now())
+
+    # Relationship to parent thread
+    thread = relationship('Thread', back_populates='messages')
+
+    # Relationship to sender
+    sender = relationship('User', back_populates='messages')
+
+    def __repr__(self) -> str:
+        return f"ThreadMessage(id={self.id!r}, content={self.content!r}, created_at={self.created_at!r}, sender_id={self.sender_id!r}, thread_id={self.thread_id!r})"
