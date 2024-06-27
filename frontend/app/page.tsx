7b58e0eccd75ccb8995a8b4f96248b4a1d0f3f5a
Jarrod <jtomasek@stedwards.edu>
2024-06-27 15:03:38 -0500

    starting fe scaffolding

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,10 +1,3 @@
-import Image from "next/image";
-
-type User = {
-  id: string;
-  name: string;
-};
-
 const apiUrl = process.env.NEXT_PUBLIC_API_URL;
 
 export default async function Home() {
@@ -14,8 +7,51 @@ export default async function Home() {
   );
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center pt-24">
       Hello, {user.name}!
+      <div className="flex flex-row gap-6">
+        <div className="flex flex-col gap-2">
+          <button
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+            type="button"
+          >
+            Start new thread
+          </button>
+          <div className="flex justify-center items-center bg-white shadow-md rounded px-8 pt-6 pb-8 ">
+            Thread 1
+          </div>
+          <div className="flex justify-center items-center bg-white shadow-md rounded px-8 pt-6 pb-8 ">
+            Thread 2
+          </div>
+        </div>
+
+        <div className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+          <form>
+            <div className="mb-4">
+              <label
+                className="block text-gray-700 text-sm font-bold mb-2"
+                htmlFor="message"
+              >
+                Message
+              </label>
+              <input
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                id="message"
+                type="text"
+                placeholder="Enter your message"
+              />
+            </div>
+            <div className="mb-6 text-center">
+              <button
+                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                type="submit"
+              >
+                Submit
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
     </main>
   );
 }
